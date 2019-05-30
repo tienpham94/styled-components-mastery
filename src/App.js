0bab15ac3c9bd83587185cd594d696a20bee913b
Tien Pham <tiendev94@gmail.com>
2019-05-30 19:48:14 +0300

    org 2

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import React from "react";
-import styled, { css } from "styled-components";
-import logo from "./logo.svg";
+
 import GlobalStyle from "./Global";
-import { Heading, Button, CancelButton } from "./elements";
-
-const AppWrapper = styled.div`
-  header {
-    background: teal;
-  }
-  ${Button} {
-    margin-bottom: 2rem;
-  }
-`;
-
-const Fake = ({ className }) => (
-  <div className>
-    <h2>I'm a fake component</h2>
-  </div>
-);
-
-const StyledFake = styled(Fake)`
-  color: red;
-`;
+import Header from "./layouts/Header";
 
 function App() {
   return (
-    <AppWrapper>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Heading>
-          Edit <code>src/App.js</code> and save to reload.
-        </Heading>
-        <Button>Save</Button>
-        <CancelButton top="100">Cancel</CancelButton>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
       <GlobalStyle />
-    </AppWrapper>
+    </div>
   );
 }
 
